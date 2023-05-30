541132bf181657855dd1a84fd1a1478f13e5af8c
Piyush Tyagi <tyagipiyush2233@gmail.com>
2023-05-30 12:53:42 +0530

    fixing bug1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logo2 from "../images/logo2.png";
+// import logo2 from "../images/logo2.png";
 import { BsSearch } from "react-icons/bs";
 import Search from "./Search";
 import { Link, useNavigate } from "react-router-dom";
